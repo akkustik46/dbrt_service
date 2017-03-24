d982bcdd47f437cbff8729bac0aff6546096dd7e
Sergey Kutsak <sergey.kutsak@uvoteam.com>
2017-03-25 01:02:56 +0200

    bug fix when add client and php7 support

--- add/client.php
+++ add/client.php
@@ -2,9 +2,9 @@
 session_start();
 include('../top2.php');
 require('../config.php');
-mysql_connect(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD);
-mysql_select_db('dbrt_garage');
-mysql_query("SET NAMES 'utf8'");
+$db=mysqli_connect(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD);
+mysqli_select_db($db,'dbrt_garage');
+mysqli_query($db,"SET NAMES 'utf8'");
 
 //$loc_lst_query=mysql_query("SELECT locations.id, locations.building, buildings.room FROM locations ORDER BY locations.building, buildings.room");
 //$phones_lst_query=mysql_query("SELECT * FROM phones WHERE (phones.num RLIKE '^380'or phones.num RLIKE '---') order by phones.num");
@@ -13,12 +13,14 @@ mysql_query("SET NAMES 'utf8'");
 ?>
 <form action="client-proc.php" method="post">
 Новый клиент<br>
+<input type="hidden" name="balance" size="10" value="0">
 <table border=0>
 <tr><td>Имя:</td><td><input type="text" name="name" size="30" value=""></td></tr>
 <tr><td>Тел1.:</td><td><input type="text" name="tel1" size="10" value=""></td></tr>
 <tr><td>Тел2.:</td><td><input type="text" name="tel2" size="10" value=""></td></tr>
-<tr><td>Скидка на работы:</td><td><input type="text" name="work_disc" size="3" value="">%</td></tr>
-<tr><td>Скидка на товары:</td><td><input type="text" name="shop_disc" size="3" value="">%</td></tr>
+<tr><td>Скидка на работы:</td><td><input type="text" name="work_disc" size="3" value='0'>%</td></tr>
+<tr><td>Скидка на товары:</td><td><input type="text" name="shop_disc" size="3" value='0'>%</td></tr>
+
 <tr><td colspan="2">Коментарий:</td></tr>
 <tr><td colspan="2"><textarea name="comment" cols="50" rows="5"></textarea></td></tr>
 <tr><td colspan="2"><center><input type="submit" value="Добавить"></center></td></tr>
