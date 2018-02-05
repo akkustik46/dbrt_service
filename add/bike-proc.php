a39df45f8e2433236f81662c0803707e58c1566c
akkustik46 <lp@ua.fm>
2018-02-05 10:47:14 +0200

    Update bike-proc.php

--- add/bike-proc.php
+++ add/bike-proc.php
@@ -3,7 +3,7 @@
 include('../top2.php');
 require('../config.php');
 $db=mysqli_connect(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD);
-mysqli_select_db($db,'dbrt_garage');
+mysqli_select_db($db,DB_SERVER_DATABASE);
 mysqli_query($db,"SET NAMES 'utf8'");
 mysqli_query($db,"INSERT INTO bike (model, year, owner, mileage, mi_km, mileage_lastchg, status, vin, comment, created,mileage_last) 
 	    VALUES ('".$_POST['model']."', '".$_POST['year']."', '".$_POST['owner']."', '".$_POST['mileage']."', '".$_POST['mikm']."',
