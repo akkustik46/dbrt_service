29213570b1c26c6dcbce021fd48c2d8506f80b09
akkustik46 <lp@ua.fm>
2018-02-05 10:52:12 +0200

    Update prod-sale-proc.php

--- add/prod-sale-proc.php
+++ add/prod-sale-proc.php
@@ -3,7 +3,7 @@
 include('../top2.php');
 require('../config.php');
 $db=mysqli_connect(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD);
-mysqli_select_db($db,'dbrt_garage');
+mysqli_select_db($db,DB_SERVER_DATABASE);
 mysqli_query($db,"SET NAMES 'utf8'");
 //print_r($_POST);
 $nam='';
