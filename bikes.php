08aa28d487344e736642d070982bbd56bf1a8006
akkustik46 <lp@ua.fm>
2018-02-07 05:35:28 +0200

    Update bikes.php

--- bikes.php
+++ bikes.php
@@ -13,7 +13,7 @@ if (!isset($_GET['action'])) {$_GET['action']='all';}
 <br>
 <?php 
 if ($_SESSION['login']=='admin') {
- echo "<div style=\"margin-left: 50px; margin-top: 30px\">".
+ echo "<div style=\"padding-top: 10px; margin-left: 50px; margin-top: 30px\">".
 "<a href='add/bike.php' target='_blank' onClick=\"popupWin = window.open(this.href, 'AddBike', 'width=470,height=380,top=200,left=60'); popupWin.focus(); return false;\">Добавить мотоцикл</a>".
 "</div>";
     }
