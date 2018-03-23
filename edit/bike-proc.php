deedba5517e4d938f9530902bc211453291dcfd8
akkustik46 <lp@ua.fm>
2018-03-23 07:30:46 +0200

    Update bike-proc.php

--- edit/bike-proc.php
+++ edit/bike-proc.php
@@ -1,3 +1,17 @@
 <?php
-print_r($_POST);
+include('../top2.php');
+require('../config.php');
+$db=mysqli_connect(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD);
+mysqli_select_db($db,DB_SERVER_DATABASE);
+mysqli_query($db,"SET NAMES 'utf8'");
+mysqli_query($db,"UPDATE bike SET bike.owner='".$_POST['owner']."', bike.year='".$_POST['year']."', bike.mileage_last='".$_POST['mileage']."', bike.mileage_lastchg=now(), bike.mi_km='".$_POST['mikm']."', bike.vin='".$_POST['vin']."', bike.comment='".$_POST['comment']."'");
+
+
+echo "Изменено!";
+mysqli_close($db);
+include('../footer.php');
 ?>
+<script>
+var tm=1000
+window.setTimeout("opener.window.location.reload(); window.close();",tm)
+</script>
