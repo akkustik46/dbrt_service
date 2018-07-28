b3fc4a09b65a764b1fd900dc4b5eef92b8c0b0c1
akkustik46 <lp@ua.fm>
2018-07-29 01:30:37 +0300

    fix charset

--- add/task.php
+++ add/task.php
@@ -20,20 +20,20 @@ mysqli_query($db,"SET NAMES 'utf8'");
 
 <div id="pagecontent">
 
-<h2 class="tabset_label">ÐÐ¾Ð²Ð°Ñ Ð·Ð°Ð´Ð°Ñ‡Ð°</h2>
+<h2 class="tabset_label">Мотоцикл в работу</h2>
 <ul class="tabset_tabs">
-	<li class="firstchild"><a href="#bike" class="preActive active">ÐœÐ¾Ñ‚Ð¾Ñ†Ð¸ÐºÐ»</a></li><li><a class="preActive postActive" href="#works">Ð Ð°Ð±Ð¾Ñ‚Ñ‹</a></li><li><a class="preActive" href="#stepbystep">Step By Step</a></li><li><a class="" href="#notes">Notes</a></li>
+	<li class="firstchild"><a href="#bike" class="preActive active">Мотоцикл</a></li><li><a class="preActive postActive" href="#works">Работы</a></li><li><a class="preActive" href="#stepbystep">Step By Step</a></li><li><a class="" href="#notes">Notes</a></li>
 </ul>
 
 <form action="task-proc.php" method="post">
 <div id="bike" class="tabset_content tabset_content_active">
-	<h2 class="tabset_label">ÐœÐ¾Ñ‚Ð¾Ñ†Ð¸ÐºÐ»</h2>
+	<h2 class="tabset_label">Мотоцикл</h2>
 	<p><?php include('task-client.php');?></p>
 </div>
 
 
 <div id="works" class="tabset_content">
-	<h2 class="tabset_label">Ð Ð°Ð±Ð¾Ñ‚Ñ‹</h2>
+	<h2 class="tabset_label">Работы</h2>
 	<p><?php include('task-test.php');
 	    ?></p>
 </div>
@@ -52,7 +52,7 @@ mysqli_query($db,"SET NAMES 'utf8'");
 </div>
 
 
-<center><input type="submit" value="Ð”Ð¾Ð±Ð°Ð²Ð¸Ñ‚ÑŒ"></center>
+<center><input type="submit" value="Добавить"></center>
 </div>
 </form>
 <p class="byline" id="copyright">&nbsp;</p>
