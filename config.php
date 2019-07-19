4cff5ecc87fb519093ff2605023d34efdbc0a2c4
akkustik <sergey.kutsak@uvoteam.com>
2019-07-20 00:41:21 +0300

    db setup

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_SERVER', 'dbrt.mysql.tools');
-define('DB_SERVER_USERNAME', 'dbrt_service');
-define('DB_SERVER_PASSWORD', 'kxwlenkj');
-define('DB_SERVER_DATABASE', 'dbrt_service');
+define('DB_SERVER_USERNAME', 'dbrt_shamans');
+define('DB_SERVER_PASSWORD', '5o5&Bf#9hA');
+define('DB_SERVER_DATABASE', 'dbrt_shamans');
 ?>
