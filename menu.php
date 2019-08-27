09fad9e4f666162091f1b7def16538e12d4fb760
akkustik <sergey.kutsak@uvoteam.com>
2019-08-28 01:41:21 +0300

    new menu

--- menu.php
+++ menu.php
@@ -5,30 +5,32 @@ header('Refresh: 0; index.php');
 }
 ?>
 <div style="float:left">
-<ul id="nav">
-<?php /*
+<?php //<ul id="nav">
+ /*
 <li><a href="zayavki.php">Заявки</a></li>
 */ ?>
 <?php
 include('db_conn.php');
 //$dep_lst_query=mysql_query("SELECT * from departments WHERE departments.parent_id='0' ORDER BY departments.dep_name");
-
+<nav class="menu">
+<ul class="topmenu">
 ?>
 <li>
 <a href="tasks.php?action=all" class="button1">Заказы</a>
-<ul>
-<li><a href="tasks.php?action=archive" class="button1">Архив</a></li>
-</ul>
+    <ul class="submenu">
+	<li><a href="tasks.php?action=archive" class="button1">Архив</a></li>
+    </ul>
 </li>
 <li>
+
 <a href="" class="button1">Справочник</a>
-<ul>
-<li><a href="clients.php" class="button1">Клиенты</a></li>
-<li><a href="bikes.php" class="button1">Мотоциклы</a></li>
-<li><a href="models.php" class="button1">Модели</a></li>
-<li><a href="works.php" class="button1">Работы</a></li>
-<li><a href="issues.php" class="button1">Извесные проблемы</a></li>
-</ul>
+    <ul class="submenu">
+	<li><a href="clients.php" class="button1">Клиенты</a></li>
+	<li><a href="bikes.php" class="button1">Мотоциклы</a></li>
+	<li><a href="models.php" class="button1">Модели</a></li>
+	<li><a href="works.php" class="button1">Работы</a></li>
+	<li><a href="issues.php" class="button1">Извесные проблемы</a></li>
+    </ul>
 </li>
 <?php
 /*    $user_query=mysqli_query($db,"select position, restricted_parts from users where users.name='".$_SESSION['login']."'");
@@ -41,9 +43,9 @@ include('db_conn.php');
 ?>
 <li>
 <a href="prod.php" class="button1">Склад</a>
-<ul>
-<li><a href="sales.php" class="button1">Продажи за период</a></li>
-</ul>
+    <ul class="submenu">
+	<li><a href="sales.php" class="button1">Продажи за период</a></li>
+    </ul>
 </li>
 
 <li>
