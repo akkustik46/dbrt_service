9369b30ad655c9ea68a49728d0535c806ce706e7
akkustik <sergey.kutsak@uvoteam.com>
2019-12-30 07:16:25 +0200

    fixup! fixup! remove button add task in history

--- tasks.php
+++ tasks.php
@@ -12,7 +12,7 @@ include('menu.php');
 <p>
 <div style="padding-top: 10px; margin-left: -40px; margin-top: 30px">
 <?php if ($_GET['action']=='all') { 
-echo "<a href=\'add/task.php\' target=\'_blank\' onClick=\"popupWin = window.open(this.href, \'AddTask\', \'location,width=600,height=700,top=0\'); popupWin.focus(); return false;\" style=\"padding-left:90px;\"><img src=\"img/add.svg\" width=30px height=30px></a>");
+echo ("<a href=\'add/task.php\' target=\'_blank\' onClick=\"popupWin = window.open(this.href, \'AddTask\', \'location,width=600,height=700,top=0\'); popupWin.focus(); return false;\" style=\"padding-left:90px;\"><img src=\"img/add.svg\" width=30px height=30px></a>");
 } 
 ?>
 
