31d0877b3786f62166f3edcea7ac17ccf9697b83
akkustik <sergey.kutsak@uvoteam.com>
2020-01-01 16:12:12 +0200

    bike history page

--- bike-history.php
+++ bike-history.php
@@ -8,6 +8,7 @@ include('menu.php');
 //$db=mysqli_connect(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD);
 
 $tasks_query=mysqli_query($db,"SELECT * FROM tasks where bike=".$_GET['bike_id']);
+$action_query=mysqli_query($db,"SELECT * FROM bike_action where bike=".$_GET['bike_id']);
 $model_name=mysqli_query($db,"select (select mnf_name from mnf where id=(select mnf_id from models where id=(SELECT model FROM bike WHERE id='".$_GET['bike_id']."'))) as make, model, capacity from models where id=(SELECT model FROM bike WHERE id='".$_GET['bike_id']."')");
 $model=mysqli_fetch_array($model_name);
 		    echo ("<br><p><div style=\"padding-top: 10px; margin-left: 100px; margin-top: 30px;font-size: x-large;\">".$model['make']." ".$model['model']." ".$model['capacity']."</div></p>");
@@ -17,7 +18,7 @@ while ($tasks_lst = mysqli_fetch_array($tasks_query)) {
 				'date' => $tasks_lst['date_create'],
 				'comment' => $tasks_lst['comment'],
 				'client' => $tasks_lst['client'],
-				'bike' => $tasks_lst['mileage'],
+				'mileage' => $tasks_lst['mileage'],
 				'status' => $tasks_lst['status'],
 				'date_end' => $tasks_lst['date_end'],
 				'date_change' => $tasks_lst['date_change']);
@@ -29,7 +30,16 @@ $bg='#ddddee';
 $bg='#ccccee';
 }
 $x++;
-
+echo $tasks_lst['mileage']." ".$tasks_lst['date_change']."<br>";
 }
 
+while ($action_lst = mysqli_fetch_array($action_query)) {
+	$action_array[] = array('old' => $action_lst['old'],
+				'new' => $action_lst['new'],
+				'type' => $action_lst['type'],
+				'mileage' => $action_lst['mileage'],
+				'date_change' => $action_lst['date_change']);
+
+echo $action_lst['mileage']." ".$action_lst['old']." ".$action_lst['new']." ".$action_lst['type']." ".$action_lst['date_change']."<br>";
+}
 ?>
