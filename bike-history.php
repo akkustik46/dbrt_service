905274b9aca86fd5cbe04e2ff75ea5a7ca00ffef
akkustik <sergey.kutsak@uvoteam.com>
2020-01-01 08:56:52 +0200

    fixup! add bike history page

--- bike-history.php
+++ bike-history.php
@@ -8,7 +8,7 @@ include('menu.php');
 $db=mysqli_connect(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD);
 
 $tasks_query=mysqli_query($db,"SELECT * FROM tasks where bike=".$_GET['bike_id']);
-$model_name=mysqli_query($db,"select (select mnf_name from mnf where id=(select mnf_id from models where id=(SELECT model FROM bike WHERE id='".$_GET['bike_id']."'))) as make, model, capacity from models where id=(SELECT modelFROM bike WHERE id='".$_GET['bike_id']."')");
+$model_name=mysqli_query($db,"select (select mnf_name from mnf where id=(select mnf_id from models where id=(SELECT model FROM bike WHERE id='".$_GET['bike_id']."'))) as make, model, capacity from models where id=(SELECT model FROM bike WHERE id='".$_GET['bike_id']."')");
 $model=mysqli_fetch_array($model_name);
 print_r($model);
 		    echo ("<div style=\"padding-top: 10px; margin-left: 200px; margin-top: 30px;font-size: x-large;\">".$model['make']." ".$model['model']." ".$model['capacity']."</div>"); 
