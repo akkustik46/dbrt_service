5515b05293cce4f62f0c0f86350c287cff14b23f
akkustik <sergey.kutsak@uvoteam.com>
2020-01-16 10:43:02 +0200

    make pdf bill tamplate

--- bill.php
+++ bill.php
@@ -99,6 +99,7 @@ $pdf->writeHTML($html, true, false, true, false, '');
 
 // Close and output PDF document
 // This method has several options, check the source code documentation for more information.
+ob_end_clean();
 $pdf->Output('bill.pdf', 'I');
 
 //============================================================+
