b12503df25e93cb965a19105e386dbf666a7e27c
akkustik <sergey.kutsak@uvoteam.com>
2020-01-16 13:00:19 +0200

    make pdf bill tamplate

--- bill.php
+++ bill.php
@@ -124,11 +124,11 @@ $pdf->Write('1', 'Виконані роботи: ');
 $pdf->SetFont('dejavusans', '', 10, '', true);
 $pdf->SetXY (20,57);
 
-$html = '<table cellspacing="1" cellpadding="1" border="1" width="100%">
+$html = '<table cellspacing="1" cellpadding="1" border="1">
 	<tr>
-	    <td width="5%"><b>№</b></td>
+	    <td width="10"><b>№</b></td>
 	    <td><b>Найменування робіт</b></td>
-	    <td width="10%"><b>Ціна</b></td>
+	    <td width="20"><b>Ціна</b></td>
 	</tr>';
 
 
@@ -142,11 +142,11 @@ while($task_wrk_lst=mysqli_fetch_array($task_wrk_query)) {
                                 'status'=>$task_wrk_lst['status']);
 $wrk=mysqli_query($db,"select works_groups.name as wgr_name, works_types.name as wrk_name from works_groups,works_types where works_groups.id=(SELECT  group_id FROM `works_types` WHERE id='".$task_wrk['type_id']."') and works_types.id='".$task_wrk['type_id']."'");
 $wrk=mysqli_fetch_array($wrk);
-$html.='<tr><td width="5%">';
+$html.='<tr><td width="10">';
 $html.=$i;
 $html.='</td><td>';
 $html.=$wrk['wrk_name'];
-$html.='</td><td width="15%" align="right">';
+$html.='</td><td width="20" align="right">';
 $html.=$task_wrk_lst['price'];
 $html.='</td></tr>';
 $wrk_sum=$wrk_sum+$task_wrk_lst['price'];
