530ff98bc0304f4b87f594b548ab26d720f5a54f
akkustik <sergey.kutsak@uvoteam.com>
2020-01-16 11:30:16 +0200

    make pdf bill tamplate

--- bill.php
+++ bill.php
@@ -99,7 +99,7 @@ $pdf->SetFont('dejavusans', 'B', 10, '', true);
 $pdf->SetXY (90,45);
 $pdf->Write('1', 'Пробіг: ');
 $pdf->SetFont('dejavusans', '', 10, '', true);
-$pdf->SetXY (110,45);
+$pdf->SetXY (100,45);
 $pdf->Write('1', $payment['mileage']);
 
 $pdf->SetFont('dejavusans', 'B', 10, '', true);
