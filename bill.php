f495580d1fab1c73d2a80d3ec93354708d094386
akkustik <sergey.kutsak@uvoteam.com>
2020-01-16 12:50:33 +0200

    make pdf bill tamplate

--- bill.php
+++ bill.php
@@ -124,10 +124,9 @@ $pdf->Write('1', 'Виконані роботи: ');
 $pdf->SetFont('dejavusans', '', 10, '', true);
 $pdf->SetXY (20,57);
 
-$html = '<<EOD 
-	<table cellspacing="1" cellpadding="1" border="1">
+$html = '<table cellspacing="1" cellpadding="1" border="1">
 	<tr>
-	    <td width=10>№</td>
+	    <td width="10">№</td>
 	    <td>Найменування робіт</td>
 	    <td>Ціна</td>
 	</tr>';
@@ -143,7 +142,7 @@ while($task_wrk_lst=mysqli_fetch_array($task_wrk_query)) {
                                 'status'=>$task_wrk_lst['status']);
 $wrk=mysqli_query($db,"select works_groups.name as wgr_name, works_types.name as wrk_name from works_groups,works_types where works_groups.id=(SELECT  group_id FROM `works_types` WHERE id='".$task_wrk['type_id']."') and works_types.id='".$task_wrk['type_id']."'");
 $wrk=mysqli_fetch_array($wrk);
-$html.='<tr><td width=10>';
+$html.='<tr><td width="10">';
 $html.=$i;
 $html.='</td><td>';
 $html.=$wrk['wrk_name'];
