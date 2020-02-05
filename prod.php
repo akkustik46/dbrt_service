1254f6116e4ef83d418e285eeb530d4de64c1fc0
akkustik <sergey.kutsak@uvoteam.com>
2020-02-05 04:39:25 +0200

    prod table round

--- prod.php
+++ prod.php
@@ -114,7 +114,7 @@ $x++;
     <?php if ($prod_lst['currency']!=1) {
 			$currency=mysqli_query($db, "SELECT name,value FROM currency where id='".$prod_lst['currency']."'");
 			$currency=mysqli_fetch_array($currency);
-			echo ("<p title=".round($prod_lst['price_in']*$currency['value'],2).">".round($prod_lst['price_out']*$currency['value'],-1)."</p>");
+			echo ("<p title=".round($prod_lst['price_in']*$currency['value'],2).">".round($prod_lst['price_out']*$currency['value'],-1,PHP_ROUND_UP)."</p>");
 	    } ?>
     </td>
     <td bgcolor=<?php echo $bg; ?> align=right>
