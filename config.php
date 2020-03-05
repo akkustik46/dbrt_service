23e884e29794dc6719d5e81377c2aac9ef81f261
akkustik <sergey.kutsak@uvoteam.com>
2020-03-05 15:21:49 +0200

    update config

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_SERVER', 'dbrt.mysql.tools');
-define('DB_SERVER_USERNAME', 'dbrt_service');
-define('DB_SERVER_PASSWORD', 'kxwlenkj');
-define('DB_SERVER_DATABASE', 'dbrt_service');
+define('DB_SERVER', 'sql164.your-server.de');
+define('DB_SERVER_USERNAME', 'service');
+define('DB_SERVER_PASSWORD', 'NykBTjnT8e26LMiW');
+define('DB_SERVER_DATABASE', 'service');
 ?>
