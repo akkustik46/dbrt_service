6e5db4c09e318b3a02be5b472ae7d35383c40cf1
akkustik <sergey.kutsak@uvoteam.com>
2020-03-05 14:17:52 +0200

    change folder

--- pull.php
+++ pull.php
@@ -3,7 +3,7 @@ if('shoxaey2do1Eol6' == $_GET['key']) {
     echo '<pre> Update...' . PHP_EOL;
 
  $dir = '/usr/www/users/dbrtac/service';
-///    $dir = '/home/dbrt/dbrt.com.ua/service/';
+//    $dir = '/home/dbrt/dbrt.com.ua/service/';
     exec("cd $dir && /usr/bin/git pull 2>&1", $output);
     $output = [];
     //exec("cd $dir && /usr/bin/git fetch --all 2>&1", $output);
