2fc14dbe09b9160bf42cfb0579436ba63de921f7
akkustik <sergey.kutsak@uvoteam.com>
2020-04-02 20:51:52 +0300

    add model modification

--- tasks.php
+++ tasks.php
@@ -65,9 +65,9 @@ $x++;
 <tr>
 <td><?php echo ($tasks_lst['id']); ?></td>
 <td style="cursor:pointer;" onClick="window.open('bike-history.php?bike_id=<?php echo ($tasks_lst['bike']);?>', '_self');">
-<?php $model_name=mysqli_query($db,"select (select mnf_name from mnf where id=(select mnf_id from models where id=(SELECT model FROM bike WHERE id='".$tasks_lst['bike']."'))) as make, model, capacity from models where id=(SELECT model FROM bike WHERE id='".$tasks_lst['bike']."')");
+<?php $model_name=mysqli_query($db,"select (select mnf_name from mnf where id=(select mnf_id from models where id=(SELECT model FROM bike WHERE id='".$tasks_lst['bike']."'))) as make, modification, model, capacity from models where id=(SELECT model FROM bike WHERE id='".$tasks_lst['bike']."')");
 $model=mysqli_fetch_array($model_name);
-		    echo ($model['make']." ".$model['model']." ".$model['capacity']); ?></td>
+		    echo ($model['make']." ".$model['model'].$model['capacity'].$model['modification']); ?></td>
 <td><?php $client_query=mysqli_query($db,"SELECT username FROM clients where id='".$tasks_lst['client']."'");
 	    $client=mysqli_fetch_array($client_query);
 echo ($client['username']); ?></td>
